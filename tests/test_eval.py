ed49433ca3c31b728cc3fc27540b909d1055013b
Mysterious Ben <datascience@tuta.io>
2020-10-28 07:06:45 +0300

    style: remove whitespace

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -19,4 +19,3 @@ def test_TimeSeriesSplit():
     cv2 = TimeSeriesSplit(test_period_len=0.5)
     scores2 = cross_validate(m, X, y, scoring=['neg_mean_squared_error'], cv=cv2)
     np.testing.assert_allclose(scores['test_neg_mean_squared_error'], scores2['test_neg_mean_squared_error'])
-
