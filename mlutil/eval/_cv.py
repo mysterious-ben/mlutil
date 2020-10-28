d42c648aaf231681e578feae247629e13ae933e4
Mysterious Ben <datascience@tuta.io>
2020-10-28 08:21:26 +0300

    major: add __repr__ to TimeSeriesSplit

--- mlutil/eval/_cv.py
+++ mlutil/eval/_cv.py
@@ -1,72 +1,77 @@
 from typing import Optional, Union
+from mlutil._util import SimpleRepr
 
 
-class TimeSeriesSplit:
+class TimeSeriesSplit(SimpleRepr):
     """Split data for nested (rolling) time-series cross-validation"""
 
-    def __init__(self,
-                 test_period_len: Union[int, float],
-                 train_period_len: Optional[Union[int, float]] = None,
-                 n_predictions: Union[int, float] = 1,
-                 prediction_lag: Union[int, float] = 0,
-                 prediction_step: Union[int, float] = 1,
-                 leave_n_first_points: Union[int, float] = 0,
-                 leave_n_last_points: Union[int, float] = 0):
+    def __init__(
+        self,
+        test_size: Union[int, float] = 0.5,
+        train_size: Optional[Union[int, float]] = None,
+        predict_size: Union[int, float] = 1,
+        predict_step: Union[int, float] = 1,
+        predict_lag: Union[int, float] = 0,
+        drop_first_points: Union[int, float] = 0,
+        drop_last_points: Union[int, float] = 0,
+    ):
         """
-        :param test_period_len: Total number of points in the test period
-        :param train_period_len: Maximum number of points in the train period
-            *None* means all points will be used
-        :param n_predictions: Predict <x> points ahead
-            E.g., *1* means that 1 value will be forecasted in every CV fold
-        :param prediction_step: Place CV folds every <x> points
-            A value greater than 1 will help to speed up CV
-        :param prediction_lag: Lag predictions by <x> points
-            A value greater than 0 means that there is a gap between the train and test period
-            Must be zero for autoregressive models
+        :param test_size: total number of points in the test period
+        :param train_size: maximum number of points in the train period
+            None - all available data points will be used for training
+        :param predict_size: predict <x> points ahead at once
+        :param predict_step: place CV folds every <x> points
+        :param predict_lag: lag predictions by <x> points
+            a non-zero value creates a gap between the train and test periods
+        :param drop_first_points: do not use first <x> points
+        :param drop_last_points: do not use last <x> points
         """
 
-        assert test_period_len > 0
-        assert (train_period_len is None) or (test_period_len > 0)
-        assert n_predictions > 0
-        assert prediction_lag >= 0
-        assert leave_n_first_points >= 0
-        assert leave_n_last_points >= 0
+        assert test_size > 0
+        assert (train_size is None) or (test_size > 0)
+        assert predict_size > 0
+        assert predict_step > 0
+        assert predict_lag >= 0
+        assert drop_first_points >= 0
+        assert drop_last_points >= 0
 
-        self.test_period_len = test_period_len
-        self.train_period_len = train_period_len
-        self.n_predictions = n_predictions
-        self.prediction_step = prediction_step
-        self.prediction_lag = prediction_lag
-        self.leave_n_first_points = leave_n_first_points
-        self.leave_n_last_points = leave_n_last_points
+        self.test_size = test_size
+        self.train_size = train_size
+        self.predict_size = predict_size
+        self.predict_step = predict_step
+        self.predict_lag = predict_lag
+        self.drop_first_points = drop_first_points
+        self.drop_last_points = drop_last_points
 
     @staticmethod
-    def _cv_gen(last_idx: int,
-                test_period_len: int,
-                train_period_len: Optional[int],
-                n_predictions: int,
-                prediction_lag: int,
-                prediction_step: int,
-                leave_n_first_points: int,
-                leave_n_last_points: int):
-        last_end_test_idx = last_idx - leave_n_last_points
-        last_end_train_idx = last_end_test_idx - n_predictions - prediction_lag
-        first_end_train_idx = last_end_train_idx - test_period_len + prediction_step
-        first_end_train_idx = first_end_train_idx + (last_end_train_idx - first_end_train_idx) % prediction_step
-        if first_end_train_idx < leave_n_first_points:
+    def _cv_gen(
+        last_idx: int,
+        test_size: int,
+        train_size: Optional[int],
+        predict_size: int,
+        predict_lag: int,
+        predict_step: int,
+        drop_first_points: int,
+        drop_last_points: int
+    ):
+        last_end_test_idx = last_idx - drop_last_points
+        last_end_train_idx = last_end_test_idx - predict_size - predict_lag
+        first_end_train_idx = last_end_train_idx - test_size + predict_step
+        first_end_train_idx = first_end_train_idx + (last_end_train_idx - first_end_train_idx) % predict_step
+        if first_end_train_idx < drop_first_points:
             raise AssertionError(f"First training interval is empty: "
-                                 f"{first_end_train_idx} < {leave_n_first_points}")
+                                 f"{first_end_train_idx} < {drop_first_points}")
 
         for end_train_idx in range(first_end_train_idx, last_end_train_idx + 1,
-                                   prediction_step):
-            if train_period_len is not None:
-                start_train_idx = max(end_train_idx - train_period_len + 1, leave_n_first_points)
+                                   predict_step):
+            if train_size is not None:
+                start_train_idx = max(end_train_idx - train_size + 1, drop_first_points)
             else:
-                start_train_idx = leave_n_first_points
+                start_train_idx = drop_first_points
             train_ids = slice(start_train_idx,
                               min(end_train_idx, last_idx) + 1)
-            test_ids = slice(end_train_idx + prediction_lag + 1,
-                             min(end_train_idx + prediction_lag + n_predictions, last_idx) + 1)
+            test_ids = slice(end_train_idx + predict_lag + 1,
+                             min(end_train_idx + predict_lag + predict_size, last_idx) + 1)
             yield train_ids, test_ids
 
     @staticmethod
@@ -84,15 +89,15 @@ class TimeSeriesSplit:
         n_points = len(X)
         return self._cv_gen(
             last_idx=n_points - 1,
-            test_period_len=self._to_points(self.test_period_len, n_points),
-            train_period_len=self._to_points(self.train_period_len, n_points),
-            n_predictions=self._to_points(self.n_predictions, n_points),
-            prediction_step=self._to_points(self.prediction_step, n_points),
-            prediction_lag=self._to_points(self.prediction_lag, n_points),
-            leave_n_first_points=self._to_points(self.leave_n_first_points, n_points),
-            leave_n_last_points=self._to_points(self.leave_n_last_points, n_points),
+            test_size=self._to_points(self.test_size, n_points),
+            train_size=self._to_points(self.train_size, n_points),
+            predict_size=self._to_points(self.predict_size, n_points),
+            predict_step=self._to_points(self.predict_step, n_points),
+            predict_lag=self._to_points(self.predict_lag, n_points),
+            drop_first_points=self._to_points(self.drop_first_points, n_points),
+            drop_last_points=self._to_points(self.drop_last_points, n_points),
         )
  
     def get_n_splits(self, X, y=None, groups=None):
-        n_splits = self.test_period_len // self.prediction_step
+        n_splits = self.test_size // self.predict_step
         return n_splits
--- tests/test_eval.py
+++ tests/test_eval.py
@@ -12,10 +12,15 @@ def test_TimeSeriesSplit():
     X = np.vstack([np.random.normal(size=100), np.random.normal(size=100)]).T
     y = np.random.normal(size=100)
     m = LinearRegression()
-    cv = TimeSeriesSplit(test_period_len=50)
+    cv = TimeSeriesSplit(test_size=50)
     scores = cross_validate(m, X, y, scoring=['neg_mean_squared_error'], cv=cv)
     assert len(scores['test_neg_mean_squared_error']) > 0
 
-    cv2 = TimeSeriesSplit(test_period_len=0.5)
+    cv2 = TimeSeriesSplit(test_size=0.5)
     scores2 = cross_validate(m, X, y, scoring=['neg_mean_squared_error'], cv=cv2)
     np.testing.assert_allclose(scores['test_neg_mean_squared_error'], scores2['test_neg_mean_squared_error'])
+
+
+def test_TimeSeriesSplit_repr():
+    cv = TimeSeriesSplit(test_size=50, train_size=20)
+    assert str(cv) == 'TimeSeriesSplit(test_size=50, train_size=20)'
