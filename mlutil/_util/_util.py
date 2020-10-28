3f286f8b9437dce05777fb9787a87cc859cd493b
Mysterious Ben <datascience@tuta.io>
2020-10-28 08:25:43 +0300

    fix: SimpleRepr now works with non-default parameters

--- mlutil/_util/_util.py
+++ mlutil/_util/_util.py
@@ -6,8 +6,7 @@ class SimpleRepr:
     """A mixin implementing a simple __repr__."""
     def __repr__(self):
         sig = inspect.signature(self.__init__)
-        default_attrs = {k: v.default for k, v in sig.parameters.items()
-                         if v.default != inspect.Parameter.empty}
+        default_attrs = {k: v.default for k, v in sig.parameters.items()}
         actual_attrs = self.__dict__
         selected_attrs = {k: actual_attrs[k] for k, v in default_attrs.items()
                           if (k in actual_attrs) and (actual_attrs[k] != v)}
